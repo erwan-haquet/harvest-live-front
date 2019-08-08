cc58fc8f475e4e24d89caa9aac1c58dcd9ff764d
Aurélien Grenet <aurel.grenet@gmail.com>
2019-08-08 16:18:40 +0200

    Adding the files I deleted localy after the review

--- src/components/observation/Wall/observationList/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ObservationContainer from '../observation/observationContainer';
-
-const ObservationList = ({observations}) =>
-    observations.map((observation)=>(
-        <ObservationContainer observations={observation} />
-    ));
-
-    export default ObservationList;
